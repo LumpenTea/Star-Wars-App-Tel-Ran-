0544c8e22116faa13d74a4659e68e14ee5f60eb2
Stanislav Shestak <stastalmid@gmail.com>
2022-07-11 22:40:32 +0300

    Navigation component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,9 @@
+import Navigation from "./Navigation";
+
 const Header = () =>{
     return(
         <header>
-            <nav className='fixed-top mt-2 ms-4'>
-                <ul className='nav'>
-                    <li className='nav-item btn btn-danger mx-1'>Home</li>
-                    <li className='nav-item btn btn-danger mx-1'>About</li>
-                    <li className='nav-item btn btn-danger mx-1'>Star wars</li>
-                    <li className='nav-item btn btn-danger mx-1'>Contact</li>
-                </ul>
-            </nav>
+            <Navigation />
             <h1 className='text-center py-4'>Luke Skywalker</h1>
         </header>
     )
